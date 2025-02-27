e5855c8b3ecb579ead60d90cec2c72c7bad713b8
Nagyla Freires <148262968+NagylaFr@users.noreply.github.com>
2025-02-27 19:04:04 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-09/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Conectado ao MongoDB'))
+.catch(err => console.log(err));
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
